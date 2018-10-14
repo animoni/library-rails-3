c7d1a3ae1b176c4df2047c385d963931f9b60438
Aniko T <a.nitto309@gmail.com>
2018-10-14 12:51:54 +0000

    Filled out the index,show,new,create,edit,update methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,46 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+  # binding.pry
+    @book = Book.find_by(id: params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render new_book_path
+    end
   end
 
   def edit
+    #binding.pry
+    @book = Book.find_by(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      redirect_to books_path
+    else
+      render edit
+    end
   end
 
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :description, author_id: :nil)
   end
 end
