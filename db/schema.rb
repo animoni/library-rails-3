8efd284e5b16e6bf858a6f39eeeebf637ff37f52
Aniko T <a.nitto309@gmail.com>
2018-10-14 13:07:51 +0000

    tried to change the schema,with no success, needs to read upon how to change colums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_203729) do
+ActiveRecord::Schema.define(version: 2018_10_13_205543) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
