7e849270bdd09fa2510e2f1efc489e5bb6fe00d7
Aniko T <a.nitto309@gmail.com>
2018-10-14 12:54:15 +0000

    Added validation to the Book model, set the author_id to optimal, while I don't have the aouthors.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
-
-
 class Book < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, optional: true
   
+  validates :title, presence: :true, length: { maximum: 50 }
+  validates :description, presence: :true, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
 
-  
 end
